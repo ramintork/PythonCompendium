893e79bc6bab54c0ddba38a4cbd8eb1903c97975
Ramin Tork <38301094+ramintork@users.noreply.github.com>
2025-06-19 11:16:38 +0100

    Delete python-3-classes-object-oriented-programming directory

--- python-3-classes-object-oriented-programming/05/demos/3. Inspecting Class Relationships/object_class.py
+++
@@ -1,7 +0,0 @@
-class Employee(object):
-    pass
-
-
-e = Employee()
-print(repr(e))
-# This is the same as just print(e) in this case
--- python-3-classes-object-oriented-programming/07/demos/4. What's next/main.py
+++
@@ -1,7 +0,0 @@
-from employee import Employee
-from project import Project
-
-
-p = Project("Django app", 20000, "Globomantics")
-e = Employee("Ji-Soo", 38, 1000, p)
-print(e.project)
